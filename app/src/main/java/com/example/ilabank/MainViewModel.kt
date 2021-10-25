7acff6e13d902a20775587d76c3e1ee702320673
krunal213 <krunalmkathikar1993@gmail.com>
2021-10-25 03:10:25 -0700

    Added New Item In List

--- app/src/main/java/com/example/ilabank/MainViewModel.kt
+++ app/src/main/java/com/example/ilabank/MainViewModel.kt
@@ -19,6 +19,13 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
          mens.add(SubSection(R.drawable.men_perfume,"perfume"))
          mens.add(SubSection(R.drawable.men_watch,"watch"))
          mens.add(SubSection(R.drawable.men_shoes,"shoes"))
+         mens.add(SubSection(R.drawable.men_cream,"cream"))
+         mens.add(SubSection(R.drawable.men_pants,"pants"))
+         mens.add(SubSection(R.drawable.men_sandal,"sandal"))
+         mens.add(SubSection(R.drawable.men_shirt,"shirt"))
+         mens.add(SubSection(R.drawable.men_trimmer,"trimmer"))
+         mens.add(SubSection(R.drawable.men_wallets,"wallet"))
+         mens.add(SubSection(R.drawable.men_jeans,"jeans"))
          sections.add(SectionType(0,R.drawable.men,mens))
 
          val womens : ArrayList<SubSection> = ArrayList()
